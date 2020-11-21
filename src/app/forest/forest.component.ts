186fbb580396da2fc3d31c8ddefc14eb44976926
Owen <owenshen24@gmail.com>
2020-11-20 23:31:50 -0800

    added stats

--- src/app/forest/forest.component.ts
+++ src/app/forest/forest.component.ts
@@ -15,8 +15,11 @@ export class ForestComponent implements OnInit {
   forestID: string;
   earnedTreeBalance: BigNumber;
   stakedTokenBalance: BigNumber;
-  availableStakeTokenBalance: BigNumber;
   totalTVL: BigNumber;
+  poolProportion: BigNumber;
+  availableStakeTokenBalance: BigNumber;
+  poolTreeBalance: BigNumber;
+  predictedTreeAmount: BigNumber;
   url: String;
 
   constructor(public wallet: WalletService, public contract: ContractService, public constants: ConstantsService, private activatedRoute: ActivatedRoute, private modalService: NgbModal) {
@@ -39,16 +42,16 @@ export class ForestComponent implements OnInit {
   async loadData() {
     const forest = this.contract.getForest(this.forestID);
     const forestStakeToken = this.contract.getForestStakeToken(this.forestID);
+    const stakeTokenPrecision = Math.pow(10, +(await forestStakeToken.methods.decimals().call()));
 
     this.earnedTreeBalance = new BigNumber(await forest.methods.earned(this.wallet.userAddress).call()).div(this.constants.TREE_PRECISION);
-    const stakeTokenPrecision = Math.pow(10, +(await forestStakeToken.methods.decimals().call()));
     this.stakedTokenBalance = new BigNumber(await forest.methods.balanceOf(this.wallet.userAddress).call()).div(stakeTokenPrecision);
-
     this.availableStakeTokenBalance = new BigNumber(await forestStakeToken.methods.balanceOf(this.wallet.userAddress).call()).div(stakeTokenPrecision);
-
     this.totalTVL = new BigNumber(await this.contract.getForestStakeToken(this.forestID).methods.balanceOf(this.contract.getForestAddress(this.forestID)).call()).div(stakeTokenPrecision);
-
+    this.poolProportion = this.stakedTokenBalance.div(this.totalTVL);
     this.url = "https://etherscan.io/address/" + this.contract.getForestAddress(this.forestID);
+    this.poolTreeBalance = new BigNumber(await this.contract.getNamedToken("TREE").methods.balanceOf(this.contract.getForestAddress(this.forestID)).call()).div(stakeTokenPrecision);
+    this.predictedTreeAmount = this.poolTreeBalance.multipliedBy(this.poolProportion);
   }
 
   resetData() {
@@ -56,6 +59,9 @@ export class ForestComponent implements OnInit {
     this.stakedTokenBalance = new BigNumber(0);
     this.availableStakeTokenBalance = new BigNumber(0);
     this.totalTVL = new BigNumber(0);
+    this.poolProportion = new BigNumber(0);
+    this.poolTreeBalance = new BigNumber(0);
+    this.predictedTreeAmount = new BigNumber(0);
     this.url = "";
   }
 
